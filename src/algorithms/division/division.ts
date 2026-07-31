6029197623973488c0496d8c578de7111b19a4a4
PieIsSpy <karldeejayomandac@gmail.com>
2026-07-31 21:48:16 +0800

    feat: added input checking for dividend and divisor, and implemented state initializations

--- src/algorithms/division/division.ts
+++ src/algorithms/division/division.ts
@@ -1,4 +1,5 @@
-import type { ConversionResult } from "../conversion/conversion";
+import { convertDecimal } from "../conversion/conversion"
+import { twosComplement } from "../helpers/twosComplement"
 
 export interface DivisionStates {
     A: string,      Q: string, // states shifted left for every loop
@@ -11,26 +12,56 @@ export interface DivisionResult {
     error?: string // for givens that cannot be computed successfully
 }
 
+function isSameType(a: number|string, b: number|string) {
+    return (typeof a === 'number' && typeof b === 'number') || (typeof a === 'string' && typeof b === 'string')
+}
+
 export function division(dividend: number|string, divisor: number|string, size: number): DivisionResult {
-    /*
-        before doing algo, check if:
-            both dividend and divisor are unsigned
-            the divisor is NOT 0
+    const result: Partial<DivisionResult> = {};
+    const states: Partial<DivisionStates> = {};
+    const range = 2 ** (size-1) - 1
 
-        if check is triggered, return error only in DivisionResult
-    */
-    
-    /*
-        after initial edge case test, if the givens are decimals, convert to binary
-    */
+    // check if the operands are in the same radix
+    if (!isSameType(dividend, divisor)) {
+        return {
+            error: 'Operands are not in the same radix'
+        }
+    }
 
-    /*
-        then initialize the ff states:
-            A = 0s with length divisor.unsigned.binary.length() + 1
-            Q = divisior.unsigned.binary
-            M = dividened.unsigned.binary
-            M_2 = 2's complement of M
-    */
+    // if the operands are in decimal
+    if (typeof dividend === 'number' && typeof divisor === 'number') {
+        // check if the operands is too big
+        if (dividend > range || divisor > range) {
+            return {
+                error: 'Operands too big for data size'
+            }
+        }
+
+        // convert to binary and store the states
+        states.Q = convertDecimal(dividend.toString(), size).unsigned.binary!
+        states.A = '0'.repeat(size + 1)
+        states.M = '0' + convertDecimal(divisor.toString(), size).unsigned.binary!
+        states.M_2 = twosComplement(states.M)
+    }
+
+    if (typeof dividend === 'string' && typeof divisor === 'string') {
+        // remove leading zeros
+        const dividend_reformat = dividend.trim().replace(/^0+/, '') || '0';
+        const divisor_reformat = divisor.trim().replace(/^0+/, '') || '0';
+        
+        // check if the operands is too big
+        if (dividend_reformat.length > size || divisor_reformat.length > size) {
+            return {
+                error: 'Operands too big for data size'
+            }
+        }
+
+        // reformat binary to have appropriate leading zeros
+        states.Q = '0'.repeat(size - dividend_reformat.length + 1) + dividend_reformat
+        states.A = '0'.repeat(size + 1)
+        states.M = '0'.repeat(size - dividend_reformat.length + 2) + divisor_reformat
+        states.M_2 = twosComplement(states.M)
+    }
 
     /*
         main algo
@@ -43,14 +74,14 @@ export function division(dividend: number|string, divisor: number|string, size: 
             3. if (A is +) Q_lsb = 1
                 else Q_lsb = 0
             ------ repeat from 1 by divisor.unsigned.binary.length() ------
-            4. if (A is +) A = A + M
+            4. if (A is -) A = A + M
 
         assign the ff to DivisionResult:
             Q = Q
             R = A
         and return
 
-        prerequisites: twosComplement.ts, addition.ts
+        prerequisites: twosComplement.ts, addition.ts, toDecimal.ts
     */
 
     return {}
